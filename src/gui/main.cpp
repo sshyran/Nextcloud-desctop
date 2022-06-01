424bdca8a2e00ebc5253412dde2e01b0293d9176
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2022-06-01 12:24:27 +0200

    force software opengl on windows instead of using env variables
    should fix broken opengl on some windows
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -59,8 +59,7 @@ void warnSystray()
 int main(int argc, char **argv)
 {
 #ifdef Q_OS_WIN
-    qputenv("QT_OPENGL", "angle");
-    qputenv("QT_ANGLE_PLATFORM", "warp");
+    QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL, true);
 
     SetDllDirectory(L"");
 #endif
